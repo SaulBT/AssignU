abeda8d14f9438832cabc3c5f2b4d7b9b57f4f1f
SaulBT <barragansaul26n@gmail.com>
2025-06-04 12:50:18 -0600

    Se emepezó a construir el ServicioClases

---
+++ API/ServicioClases/Data/ClasesDbContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using ServicioClases.Models;
+
+namespace ServicioClases.Data;
+
+public partial class ClasesDbContext : DbContext
+{
+    public ClasesDbContext(DbContextOptions<ClasesDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Clase> Clase { get; set; }
+
+    public virtual DbSet<Registro> Registro { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .UseCollation("utf8mb4_0900_ai_ci")
+            .HasCharSet("utf8mb4");
+
+        modelBuilder.Entity<Clase>(entity =>
+        {
+            entity.HasKey(e => e.IdClase).HasName("PRIMARY");
+        });
+
+        modelBuilder.Entity<Registro>(entity =>
+        {
+            entity.HasKey(e => e.IdRegistro).HasName("PRIMARY");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ API/ServicioClases/Models/Clase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace ServicioClases.Models;
+
+[Table("clase")]
+[Index("Codigo", Name = "codigo_UNIQUE", IsUnique = true)]
+public partial class Clase
+{
+    [Key]
+    [Column("idClase")]
+    public int IdClase { get; set; }
+
+    [Column("codigo")]
+    [StringLength(45)]
+    public string Codigo { get; set; } = null!;
+
+    [Column("nombre")]
+    [StringLength(45)]
+    public string? Nombre { get; set; }
+
+    [Column("idDocente")]
+    public int IdDocente { get; set; }
+}

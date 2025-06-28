02f82a8c7c75af2863394b94f1875d7f5785e991
SaulBT <barragansaul26n@gmail.com>
2025-06-27 23:51:37 -0600

    Correcciones:  - Se cambió colocó el nombre CorreoElectrónico en todos los campos de    todos los DTOs relacionados al correo.  - Se cambió de Contrasena a Contrasenia en IniciarSesionDTO.

--- API/ServicioUsuarios/Data/DTOs/Alumno/AlumnoDTO.cs
+++ API/ServicioUsuarios/Data/DTOs/Alumno/AlumnoDTO.cs
@@ -6,6 +6,6 @@ public class AlumnoDTO
     public int IdAlumno { get; set; } = 0;
     public string NombreCompleto { get; set; } = string.Empty;
     public string NombreUsuario { get; set; } = string.Empty;
-    public string Correo { get; set; } = string.Empty;
+    public string CorreoElectronico { get; set; } = string.Empty;
     public int IdGradoEstudios { get; set; } = 0;
 }
